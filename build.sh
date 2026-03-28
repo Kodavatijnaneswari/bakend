fbc243213edb98f3a99c5727a4a6004b941016a0
jnaneswari <jnaneswarikodavati46@gmail.com>
2026-03-28 18:12:57 +0530

    Fix Render 502 error: Optimize dependencies and build configuration

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 # exit on error
 set -o errexit
 
-pip install torch torchvision --index-url https://download.pytorch.org/whl/cpu
-pip install -r requirements.txt
+pip install --upgrade pip
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cpu --no-cache-dir
+pip install -r requirements.txt --no-cache-dir
 
 python manage.py collectstatic --noinput
 python manage.py migrate
