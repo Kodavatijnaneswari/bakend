9bba5b2aa11150487b6d43ebe395b5f2c1666ebd
Jnaneswari Kodavati <jnaneswari.kodavati@gmail.com>
2026-03-28 15:30:33 +0530

    Perfect production configuration: enable media serving and optimize stability

--- Bone_Abnormality_Detection/urls.py
+++ Bone_Abnormality_Detection/urls.py
@@ -1,23 +1,14 @@
 """
-URL configuration for Wrist_Abnormality_Detection project.
+URL configuration for Bone_Abnormality_Detection project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from admins import views
 from users import views as u
 from users import api_views as api
@@ -49,5 +40,6 @@ urlpatterns = [
     path('adminbase/', views.adminbase, name='adminbase'),
     path('delete/<int:id>', views.delete, name='delete'),
 
-
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
